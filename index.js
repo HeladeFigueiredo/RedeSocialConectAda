64c78be060cc591b3afc2ef84448bf56ad1b9364
André Bitencourt <andredasilvabitencourt@outlook.com>
2022-11-21 17:04:46 -0300

    new file:   css/style.css 	new file:   images/feed-1.jpg 	new file:   images/feed-2.jpg 	new file:   images/feed-3.jpg 	new file:   images/feed-4.jpg 	new file:   images/feed-5.jpg 	new file:   images/feed-6.jpg 	new file:   images/feed-7.jpg 	new file:   images/profile-1.jpg 	new file:   images/profile-10.jpg 	new file:   images/profile-11.jpg 	new file:   images/profile-12.jpg 	new file:   images/profile-13.jpg 	new file:   images/profile-14.jpg 	new file:   images/profile-15.jpg 	new file:   images/profile-16.jpg 	new file:   images/profile-17.jpg 	new file:   images/profile-18.jpg 	new file:   images/profile-19.jpg 	new file:   images/profile-2.jpg 	new file:   images/profile-20.jpg 	new file:   images/profile-3.jpg 	new file:   images/profile-4.jpg 	new file:   images/profile-5.jpg 	new file:   images/profile-6.jpg 	new file:   images/profile-7.jpg 	new file:   images/profile-8.jpg 	new file:   images/profile-9.jpg 	new file:   images/social logo.png 	new file:   images/story-1.jpg 	new file:   images/story-2.jpg 	new file:   images/story-3.jpg 	new file:   images/story-4.jpg 	new file:   images/story-5.jpg 	new file:   images/story-6.jpg 	new file:   index.html 	new file:   index.js 	new file:   index2.html 	modified:   usuario.js

---
+++ index.js
@@ -0,0 +1,215 @@
+//SIDEBAR
+const menuItens = document.querySelectorAll('.menu-item');
+
+//MENSAGENS
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+//TEMA
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
+
+
+//SIDEBAR
+//REmove class active menu itens
+const changeActiveItem = () => {
+    menuItens.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItens.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();//Desmarco o ativo
+        item.classList.add('active');
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').style.display = 'block';
+            document.querySelector('#notifications .notification-count').style.display = 'none';//Oculta a contagem de notificações
+        }
+    })
+})
+
+
+//MENSAGENS
+
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            user.style.display = 'flex';
+        } else {
+            user.style.display = 'none';
+        }
+    })
+}
+
+//Busca chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+
+
+
+//CUSTOMIZAÇÃO DO TEMA
+
+//Abrir modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+//Close Modal
+const closeThemeModal = (e) => {
+    if (e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = 'none';
+    }
+}
+
+//Fechar Modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
+
+
+//Remove active do seletor de fontes
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    });
+}
+
+
+
+
+
+//Fontes
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active');
+
+        //alert('Muda tamanho');
+        if (size.classList.contains('font-size-1')) {
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        } else if (size.classList.contains('font-size-2')) {
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        } else if (size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        } else if (size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        } else if (size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-10rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');
+        }
+
+        //Modifiar elemento root
+        document.querySelector('html').style.fontSize = fontSize;
+
+    })
+
+
+})
+
+
+//remove cor ativa
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+
+//Muda cores
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        changeActiveColorClass();
+        let primaryHue;
+        if (color.classList.contains('color-1')) {
+            primaryHue = 252;
+        } else if (color.classList.contains('color-2')) {
+            primaryHue = 52;
+        } else if (color.classList.contains('color-3')) {
+            primaryHue = 352;
+        } else if (color.classList.contains('color-4')) {
+            primaryHue = 152;
+        } else if (color.classList.contains('color-5')) {
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    });
+})
+
+
+//Muda Background
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+
+}
+
+Bg1.addEventListener('click', () => {
+    // darkColorLightness = '95%';
+    // whiteColorLightness = '20%';
+    // lightColorLightness = '15%';
+    Bg1.classList.add('active');
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    window.location.reload();
+    //changeBG();
+})
+
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    Bg2.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+})
+
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    Bg3.classList.add('active');
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+})
--- usuario.js
+++ usuario.js
@@ -7,6 +7,7 @@ class Usuario {
 
     constructor(id, nome, email, nomeUsuarioCadastrado,senhaCadastrada){
         this.#id = id;
+        this.#nome = nome;
         this.#email = email;
         this.#nomeUsuarioCadastrado = nomeUsuarioCadastrado;
         this.#senhaCadastrada= senhaCadastrada;
