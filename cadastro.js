bbefe36d66d8dc74c3e59ef7b7d9be8e7382b92f
nataliarcr <nravylla@gmail.com>
2022-11-25 16:36:41 -0300

    wip: cadastro de usuário

--- cadastro.js
+++ cadastro.js
@@ -1,11 +1,23 @@
 class cadastrarUsuario{
-    constructor(){
 
+    // Recupera o valor digitado no campo de nome do formulário de cadastro
+    nomeDigitado = document.querySelector("#nomeUsuario").value;
+
+    // Recupera o valor digitado no campo de senha do formulário de cadastro
+    emailDigitado = document.querySelector("#usuarioLogin").value;
+
+    // Recupera o valor digitado no campo de senha do formulário de cadastro
+    #senhaDigitada = document.querySelector("#senhaLogin").value;
+
+    constructor(nomeDigitado, emailDigitado, senhaDigitada){
+        this.nomeDigitado = nomeDigitado;
+        this.emailDigitado = emailDigitado;
+        this.#senhaDigitada = senhaDigitada;
     }
 
     verificarSenha (){
 
-        var senhaLogin = document.getElementById("senhaLogin");
+        let senhaLogin = document.getElementById("senhaLogin");
         confirmarSenha = document.getElementById("confirmarSenha");
             
             if(senhaLogin.value != confirmarSenha.value) {
@@ -19,3 +31,5 @@ class cadastrarUsuario{
     }
     
 }
+
+document.querySelector("#botaoCadastro").onclick = cadastrarUsuario;
